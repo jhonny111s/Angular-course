ff90c3625f6f32a936ec0b9dc7f4fdcf68e96e81
Jhonny <jhonny111s@gmail.com>
2016-09-08 13:28:40 -0500

    se agrego el comportamiento para cuando haya errores en la peticion http en los controladores y se agrego a los html de home,menu y detaildish un ng if y un mensage para mostrar el error y no el contenido

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -9,11 +9,17 @@ angular.module('confusionApp')
  $scope.showDetails = false;
 
 
- $scope.dishes = [];
+ $scope.showMenu = false;
+ $scope.message = "Loading ...";
+ $scope.dishes = {};
  menuFactory.getDishes()
   .then(
    function(response) {
     $scope.dishes = response.data;
+    $scope.showMenu = true;
+   },
+   function(response) {
+    $scope.message = "Error: " + response.status + " " + response.statusText;
    }
   );
 
@@ -92,11 +98,16 @@ angular.module('confusionApp')
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
  $scope.dish = {};
+ $scope.showDish = false;
+ $scope.message = "Loading ...";
  menuFactory.getDish(parseInt($stateParams.id, 10))
   .then(
    function(response) {
     $scope.dish = response.data;
     $scope.showDish = true;
+   },
+   function(response) {
+    $scope.message = "Error: " + response.status + " " + response.statusText;
    }
   );
 
@@ -139,12 +150,17 @@ angular.module('confusionApp')
  $scope.promotion = promotion;
 
  $scope.dish = {};
+ $scope.showDish = false;
+ $scope.message = "Loading ...";
 
  menuFactory.getDish(0)
   .then(
    function(response) {
     $scope.dish = response.data;
     $scope.showDish = true;
+   },
+   function(response) {
+    $scope.message = "Error: " + response.status + " " + response.statusText;
    }
   );
 }])
