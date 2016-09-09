657567d5a75e86e24d1135fce075155305309214
Jhonny <jhonny111s@gmail.com>
2016-09-09 00:11:01 -0500

    se agrego el modulo ngresource para cambiarlo por http ya que brinda mas capacidades, se cambio el service y los controles al hacer las peticiones a dish

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -8,21 +8,9 @@ angular.module('confusionApp')
  $scope.filtText = '';
  $scope.showDetails = false;
 
-
- $scope.showMenu = false;
+ $scope.showMenu = true;
  $scope.message = "Loading ...";
- $scope.dishes = {};
- menuFactory.getDishes()
-  .then(
-   function(response) {
-    $scope.dishes = response.data;
-    $scope.showMenu = true;
-   },
-   function(response) {
-    $scope.message = "Error: " + response.status + " " + response.statusText;
-   }
-  );
-
+ $scope.dishes = menuFactory.getDishes().query();
 
  $scope.select = function(setTab) {
   $scope.tab = setTab;
@@ -97,19 +85,11 @@ angular.module('confusionApp')
 
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
- $scope.dish = {};
- $scope.showDish = false;
+ $scope.showDish = true;
  $scope.message = "Loading ...";
- menuFactory.getDish(parseInt($stateParams.id, 10))
-  .then(
-   function(response) {
-    $scope.dish = response.data;
-    $scope.showDish = true;
-   },
-   function(response) {
-    $scope.message = "Error: " + response.status + " " + response.statusText;
-   }
-  );
+ $scope.dish = menuFactory.getDishes().get({
+  id: parseInt($stateParams.id, 10)
+ });
 
 }])
 
@@ -149,20 +129,11 @@ angular.module('confusionApp')
  $scope.leader = leader;
  $scope.promotion = promotion;
 
- $scope.dish = {};
- $scope.showDish = false;
+ $scope.showDish = true;
  $scope.message = "Loading ...";
-
- menuFactory.getDish(0)
-  .then(
-   function(response) {
-    $scope.dish = response.data;
-    $scope.showDish = true;
-   },
-   function(response) {
-    $scope.message = "Error: " + response.status + " " + response.statusText;
-   }
-  );
+ $scope.dish = menuFactory.getDishes().get({
+  id: 0
+ });
 }])
 
 .controller('AboutController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
--- conFusion/app/scripts/services.js
+++ conFusion/app/scripts/services.js
@@ -3,7 +3,7 @@
 angular.module('confusionApp')
  .constant("baseURL", "http://localhost:3000/")
 
-.service('menuFactory', ['$http', 'baseURL', function($http, baseURL) {
+.service('menuFactory', ['$resource', 'baseURL', function($resource, baseURL) {
 
  var promotions = [{
    _id: 0,
@@ -17,10 +17,11 @@ angular.module('confusionApp')
  ];
 
  this.getDishes = function() {
-  return $http.get(baseURL + "dishes");
- };
- this.getDish = function(index) {
-  return $http.get(baseURL + "dishes/" + index);
+  return $resource(baseURL + "dishes/:id", null, {
+   'update': {
+    method: 'PUT'
+   }
+  });
  };
 
  // implement a function named getPromotion
